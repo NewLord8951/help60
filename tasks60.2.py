e5bedca3bd7f114b8625d90324b2d7c53f43a684
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 08:59:29 +0000

    60.2

--- tasks60.2.py
+++ tasks60.2.py
@@ -1,6 +1,13 @@
-a = int(input('Введите число:'))
-b = int(input('Введите число:'))
-c = int(input('Введите число:'))
-f = max(a, b, c)
-d = min(a, b, c)
-print(f, d)
+num1 = int(input("Введите число 1: "))
+num2 = int(input("Введите число 2: "))
+num3 = int(input("Введите число 3: "))
+
+
+def minimum_maximum(num1, num2, num3):
+    spisok = [num1, num2, num3]
+    maximum = max(spisok)
+    minimum = min(spisok)
+    return maximum, minimum
+
+
+print(minimum_maximum(num1, num2, num3))
