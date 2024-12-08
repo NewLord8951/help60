61e87fc230cd8c38f6b1b9837865b1a371f24f57
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 09:02:31 +0000

    60.9

--- tasks60.9.py
+++ tasks60.9.py
@@ -1,4 +1,15 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+n = int(input("Количество чисел в ряду: "))
+
+
 def fib(n):
-    if n < 2:
-        return n
-    return fib(n-2) + fib(n-1)
+    fib_p = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_p.append(a)
+        a, b = b, a + b
+    return fib_p
+
+
+print(fib(n))
