62c7a117d4e95cebe826403b1e8ca90365845ae8
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 09:01:40 +0000

    60.8

--- tasks60.8.py
+++ tasks60.8.py
@@ -1,5 +1,11 @@
+n = int(input("Введите натуральное число: "))
+
+
 def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+    number = 1
+    for i in range(1, n+1):
+        number = number * i
+    return number
+
+
+print(factorial(n))
