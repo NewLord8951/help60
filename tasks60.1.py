5d28abbb1bb1cdc38ccea4650746b847c2882d24
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 08:59:00 +0000

    60.1

--- tasks60.1.py
+++ tasks60.1.py
@@ -1,7 +1,15 @@
-def maximum(a, b, c):
-    if a > b and a > c:
-        return a
-    elif b > c:
-        return b
+num1 = int(input("Введите число num1: "))
+num2 = int(input("Введите число num2: "))
+num3 = int(input("Введите число num3: "))
+
+
+def maximum(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num3:
+        return num2
     else:
-        return c
+        return num3
+
+
+print(maximum(num1, num2, num3))
